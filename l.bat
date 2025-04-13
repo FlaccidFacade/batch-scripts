e26d4d6cb5377238acf6e5222685fbd07ab7fd15
FlaccidFacade <FlaccidFacade@pm.me>
2025-04-13 14:20:52 -0500

    add the "l" short hand

---
+++ l.bat
@@ -0,0 +1,6 @@
+@REM Author: Zachary Turner 
+@REM This script is a simple implementation of the Unix 'ls' command
+@REM in Windows batch scripting. It provides options for long listing format, showing hidden files,
+@echo off
+
+ls -a
