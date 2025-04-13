2feaf942dcbfb11242150463726e7da1cc0c3cdd
Zachary <zachary.glenn.turner@gmail.com>
2025-04-13 21:28:48 +0000

    upgrade to pass fail in batch file exit code

--- doesItContain.bat
+++ doesItContain.bat
@@ -2,5 +2,9 @@
 @echo off
 set str2Check=%1
 set pattern2Find=%2
-if not x%str2Check:%pattern2Find=%==x%str2Check% echo It contains bcd
+if not x%str2Check:%pattern2Find=%==x%str2Check% (
+    echo exit /b 1
+) else (
+    echo exit /b 0
+)
 endlocal
